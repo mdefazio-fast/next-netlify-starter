aa95615a9ba19067f5b8112453bf69a10748cd9e
mdefazio-fast <mdefazio.fast@gmail.com>
2021-12-08 16:27:33 -0500

    added Fast button to index.js

--- pages/index.js
+++ pages/index.js
@@ -15,6 +15,12 @@ export default function Home() {
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
+        <br>
+<form>
+    <fast-checkout-button id="fast-checkout-button"></fast-checkout-button>
+</form>
+<br>
+
       </main>
 
       <Footer />
